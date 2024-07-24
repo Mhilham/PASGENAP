b8268d3fc526d7d2f9982d84c9e80f9a0b805b47
mhilham <mhilham16@gmail.com>
2024-07-24 09:14:56 +0700

    update main.js

--- main.js
+++ main.js
@@ -65,11 +65,11 @@ export async function tambahsiswa(tanggal, nis, nama, alamat, notlpn, kelas, ket
 
 
 export async function hapusAbsensi(docId) {
-  await deleteDoc(doc(db, "absensi_siswa", docId));
+  await deleteDoc(doc(db, "Absensi_siswa", docId));
 }
 
 export async function ubahAbsensi(docId, tanggal, nis, nama, alamat, noTlpn, kelas, keterangan) {
-  await updateDoc(doc(db, "absensi_siswa", docId), {
+  await updateDoc(doc(db, "Absensi_siswa", docId), {
     tanggal: tanggal,
     nis: nis,
     nama: nama,
@@ -81,7 +81,7 @@ export async function ubahAbsensi(docId, tanggal, nis, nama, alamat, noTlpn, kel
 }
 
 export async function ambilAbsensi(docId) {
-  const docRef = await doc(db, "absensi_siswa", docId);
+  const docRef = await doc(db, "Absensi_siswa", docId);
   const docSnap = await getDoc(docRef);
 
   return await docSnap.data();
